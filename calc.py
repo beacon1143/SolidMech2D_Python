2d391d84816ca02c4f94b567d1cf6fc71722a48f
Maxim Yakovlev <beacon1143@gmail.com>
2024-07-31 11:46:10 +0300

    solving PDEs system, acoustics 1D

--- calc.py
+++ calc.py
@@ -3,19 +3,23 @@ import matplotlib.pyplot as plt
 
 # PHYSICS
 lX = 10.0
-c = -1.0
+K = 1.0
+rho = 1.0
 
 # NUMERICS
-dt = 0.01
-nSteps = 100
 nX = 100
+nSteps = 100
+cfl = 1.0
 
 # PREPROCESSING
 x = np.linspace(-0.5 * lX, 0.5 * lX, nX)
+dX = lX / (nX - 1)
+dt = cfl * dX / np.sqrt(K / rho)
 
 # INITIAL CONDITIONS
 p0 = 1.0
 p = np.exp(-p0 * x * x)
+v = np.zeros(nX + 1)
 
 plt.ion()    # interactive mode
 graph = plt.plot(x, p)[0]
@@ -23,8 +27,10 @@ plt.pause(0.1)
 
 # ACTION LOOP
 for i in range(nSteps):
-    dpdt = c * p
+    dpdt = -np.diff(v) / dX * K
     p = p + dpdt * dt
+    dvdt = -np.diff(p) / dX / rho
+    v[1:-1] = v[1:-1] + dvdt * dt
     graph.remove()
     graph = plt.plot(x, p)[0]
     plt.title(str(i+1))
