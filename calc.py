1aec7c740a53fd06b101370e835f6eb059d32466
Maxim Yakovlev <beacon1143@gmail.com>
2024-08-01 00:43:16 +0300

    solving PDEs system, elastic dynamics 1D

--- calc.py
+++ calc.py
@@ -4,21 +4,23 @@ import matplotlib.pyplot as plt
 # PHYSICS
 lX = 10.0
 K = 1.0
+G = 1.5
 rho = 1.0
 
 # NUMERICS
 nX = 100
-nSteps = 100
+nSteps = 1000
 cfl = 1.0
 
 # PREPROCESSING
 x = np.linspace(-0.5 * lX, 0.5 * lX, nX)
 dX = lX / (nX - 1)
-dt = cfl * dX / np.sqrt(K / rho)
+dt = cfl * dX / np.sqrt((K + 4.0 * G / 3.0) / rho)
 
 # INITIAL CONDITIONS
 p0 = 1.0
 p = np.exp(-p0 * x * x)
+tau = np.zeros(nX)
 v = np.zeros(nX + 1)
 
 plt.ion()    # interactive mode
@@ -29,7 +31,9 @@ plt.pause(0.1)
 for i in range(nSteps):
     dpdt = -np.diff(v) / dX * K
     p = p + dpdt * dt
-    dvdt = -np.diff(p) / dX / rho
+    dtaudt = np.diff(v) * G * 4.0 / 3.0
+    tau = tau + dtaudt * dt
+    dvdt = np.diff(-p + tau) / dX / rho
     v[1:-1] = v[1:-1] + dvdt * dt
     graph.remove()
     graph = plt.plot(x, p)[0]
