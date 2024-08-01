8aa6eaac506d2bbe34a1820dc2b3f8e02c8015f4
Maxim Yakovlev <beacon1143@gmail.com>
2024-08-01 20:55:37 +0300

    elastic dynamics 2D

--- calc.py
+++ calc.py
@@ -3,39 +3,52 @@ import matplotlib.pyplot as plt
 
 # PHYSICS
 lX = 10.0
+lY = 10.0
 K = 1.0
-G = 1.5
+G = 0.5
 rho = 1.0
 
 # NUMERICS
-nX = 100
-nSteps = 1000
-cfl = 1.0
+nX = 200
+nY = 200
+nSteps = 400
+cfl = 0.5
 
 # PREPROCESSING
 x = np.linspace(-0.5 * lX, 0.5 * lX, nX)
+y = np.linspace(-0.5 * lY, 0.5 * lY, nY)
+x, y = np.meshgrid(x, y, indexing='ij')    # 1D arrays x and y became 2D
 dX = lX / (nX - 1)
-dt = cfl * dX / np.sqrt((K + 4.0 * G / 3.0) / rho)
+dY = lY / (nY - 1)
+dt = cfl * min(dX, dY) / np.sqrt((K + 4.0 * G / 3.0) / rho)
 
 # INITIAL CONDITIONS
 p0 = 1.0
-p = np.exp(-p0 * x * x)
-tau = np.zeros(nX)
-v = np.zeros(nX + 1)
+p = np.exp(-p0 * (x * x + y * y))
+tauXX = np.zeros((nX, nY))
+tauYY = np.zeros((nX, nY))
+tauXY = np.zeros((nX - 1, nY - 1))
+vX = np.zeros((nX + 1, nY))
+vY = np.zeros((nX, nY + 1))
 
 plt.ion()    # interactive mode
-graph = plt.plot(x, p)[0]
+graph = plt.pcolormesh(x, y, p)
+plt.colorbar()
+plt.gca().set_aspect('equal')
 plt.pause(0.1)
 
 # ACTION LOOP
 for i in range(nSteps):
-    dpdt = -np.diff(v) / dX * K
-    p = p + dpdt * dt
-    dtaudt = np.diff(v) * G * 4.0 / 3.0
-    tau = tau + dtaudt * dt
-    dvdt = np.diff(-p + tau) / dX / rho
-    v[1:-1] = v[1:-1] + dvdt * dt
+    divV = np.diff(vX, 1, 0) / dX + np.diff(vY, 1, 1) / dY
+    p = p - divV * K * dt
+    tauXX = tauXX + (np.diff(vX, 1, 0) / dX - divV / 3.0) * 2.0 * G * dt
+    tauYY = tauYY + (np.diff(vY, 1, 1) / dY - divV / 3.0) * 2.0 * G * dt
+    tauXY = tauXY + (np.diff(vX[1:-1, :], 1, 1) / dY + np.diff(vY[:, 1:-1], 1, 0) / dY) * G * dt
+    dvXdt = (np.diff(-p[:, 1:-1] + tauXX[:, 1:-1], 1, 0) / dX + np.diff(tauXY, 1, 1) / dY) / rho
+    vX[1:-1, 1:-1] = vX[1:-1, 1:-1] + dvXdt * dt
+    dvYdt = (np.diff(-p[1:-1, :] + tauYY[1:-1, :], 1, 1) / dY + np.diff(tauXY, 1, 0) / dX ) / rho
+    vY[1:-1, 1:-1] = vY[1:-1, 1:-1] + dvYdt * dt
     graph.remove()
-    graph = plt.plot(x, p)[0]
+    graph = plt.pcolormesh(x, y, p)
     plt.title(str(i+1))
     plt.pause(0.001)
