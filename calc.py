270746b3d7814d0a466cdd0d91e5a80b85428b17
Maxim Yakovlev <beacon1143@gmail.com>
2024-08-04 22:53:57 +0300

    elastic statics 2D, pseudo-transient method

--- calc.py
+++ calc.py
@@ -11,8 +11,9 @@ rho = 1.0
 # NUMERICS
 nX = 200
 nY = 200
-nSteps = 400
+nSteps = 500
 cfl = 0.5
+dmp = 4.0 / nX
 
 # PREPROCESSING
 x = np.linspace(-0.5 * lX, 0.5 * lX, nX)
@@ -32,9 +33,21 @@ vX = np.zeros((nX + 1, nY))
 vY = np.zeros((nX, nY + 1))
 
 plt.ion()    # interactive mode
-graph = plt.pcolormesh(x, y, p)
-plt.colorbar()
-plt.gca().set_aspect('equal')
+fig, graph = plt.subplots(1, 2)
+gr0 = graph[0].pcolormesh(x, y, p)
+
+graph[0].set_xlim(-0.5 * lX, 0.5 * lX)
+graph[0].set_ylim(-0.5 * lY, 0.5 * lY)
+graph[0].axis('scaled')
+graph[0].set_title('p')
+fig.colorbar(gr0)
+
+gr1 = graph[1].pcolormesh(x, y, tauXX)
+fig.colorbar(gr1)
+graph[1].set_xlim(-0.5 * lX, 0.5 * lX)
+graph[1].set_ylim(-0.5 * lY, 0.5 * lY)
+graph[1].axis('scaled')
+graph[1].set_title('tau_xx')
 plt.pause(0.1)
 
 # ACTION LOOP
@@ -45,10 +58,12 @@ for i in range(nSteps):
     tauYY = tauYY + (np.diff(vY, 1, 1) / dY - divV / 3.0) * 2.0 * G * dt
     tauXY = tauXY + (np.diff(vX[1:-1, :], 1, 1) / dY + np.diff(vY[:, 1:-1], 1, 0) / dY) * G * dt
     dvXdt = (np.diff(-p[:, 1:-1] + tauXX[:, 1:-1], 1, 0) / dX + np.diff(tauXY, 1, 1) / dY) / rho
-    vX[1:-1, 1:-1] = vX[1:-1, 1:-1] + dvXdt * dt
+    vX[1:-1, 1:-1] = (1 - dmp) * vX[1:-1, 1:-1] + dvXdt * dt
     dvYdt = (np.diff(-p[1:-1, :] + tauYY[1:-1, :], 1, 1) / dY + np.diff(tauXY, 1, 0) / dX ) / rho
-    vY[1:-1, 1:-1] = vY[1:-1, 1:-1] + dvYdt * dt
-    graph.remove()
-    graph = plt.pcolormesh(x, y, p)
-    plt.title(str(i+1))
-    plt.pause(0.001)
+    vY[1:-1, 1:-1] = (1 - dmp) * vY[1:-1, 1:-1] + dvYdt * dt
+    gr0.remove()
+    gr1.remove()
+    gr0 = graph[0].pcolormesh(x, y, p)
+    gr1 = graph[1].pcolormesh(x, y, tauXX)
+    fig.suptitle(str(i+1))
+    plt.pause(0.0001)
