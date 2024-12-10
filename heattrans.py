10f5c9a5a0cf8e76e4a9f7bd23eecfcf5aa4eb24
Maxim Yakovlev <beacon1143@gmail.com>
2024-12-10 16:18:05 +0300

    three subplots instead of one for heattrans

--- heattrans.py
+++ heattrans.py
@@ -28,11 +28,19 @@ T = T0 * np.exp(-x * x - y * y)
 qx = np.zeros((nx - 1, nx - 2))
 qy = np.zeros((nx - 2, nx - 1))
 
-fig, graph = plt.subplots()
-gr = graph.pcolormesh(x, y, T, shading='auto')
-graph.axis('scaled')
-graph.set_title('T')
-fig.colorbar(gr, location='right')
+fig, graph = plt.subplots(1, 3)
+
+def make_plot(fig, plot, x, y, field, name):
+    gr = plot.pcolormesh(x, y, field, shading='auto')
+    plot.axis('scaled')
+    plot.set_title(name)
+    fig.colorbar(gr, location='bottom')
+    return gr
+
+gr = []
+gr.append(make_plot(fig, graph[0], x, y, T, 'T'))
+gr.append(make_plot(fig, graph[1], x[:-1, :-2], y[:-1, :-2], qx, 'qx'))
+gr.append(make_plot(fig, graph[2], x[:-2, :-1], y[:-2, :-1], qy, 'qy'))
 
 # ACTION LOOP
 def action_loop(i):
@@ -41,9 +49,13 @@ def action_loop(i):
     dTdt = -(np.diff(qx, 1, 0) / dx + np.diff(qy, 1, 1) / dy) / rhocp
     T[1:-1, 1:-1] = T[1:-1, 1:-1] + dTdt * dt
     fig.suptitle(str(i+1))
-    gr.set_array(T)
-    gr.set_clim([T.min(), T.max()])
+    gr[0].set_array(T)
+    gr[0].set_clim([T.min(), T.max()])
+    gr[1].set_array(qx)
+    gr[1].set_clim([qx.min(), qx.max()])
+    gr[2].set_array(qy)
+    gr[2].set_clim([qy.min(), qy.max()])
     return gr
 
-anim = ani.FuncAnimation(fig=fig, func=action_loop, frames=nsteps, interval=1, repeat=False)
+anim = ani.FuncAnimation(fig, action_loop, nsteps, interval=1, repeat=False)
 plt.show()
